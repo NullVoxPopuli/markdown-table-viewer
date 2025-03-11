f26865dc4caa044fec704f4fe4e8da3531cbedb1
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-03-11 18:44:35 -0400

    It repros less subtly

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,9 @@ import { defineConfig } from 'vite';
 import { extensions, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
-const validator = `${process.cwd()}/node_modules/ember-source/dist/packages/@glimmer/validator/index.js`;
-const tracking = `${process.cwd()}/node_modules/ember-source/dist/packages/@glimmer/tracking/index.js`;
-
 export default defineConfig({
   resolve: {
     extensions,
-    alias: {
-      '@glimmer/validator': validator,
-      '@glimmer/tracking': tracking,
-    },
   },
   plugins: [
     ember(),
